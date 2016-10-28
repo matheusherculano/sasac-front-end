ead8b9109794fd3abd0a02806c5e1b6cdefaa3bc
Matheus Herculano <RA21501871@T1-110430-57276.dinfor.uniceub>
2016-10-28 16:07:16 -0200

    correção configuração chartJS

--- sasac - front - GIT/public_html/src/app/pages/avaliacoes/graficos/grafico.module.js
+++ sasac - front - GIT/public_html/src/app/pages/avaliacoes/graficos/grafico.module.js
@@ -4,7 +4,8 @@
   angular.module('BlurAdmin.pages.avaliacao.grafico', [
       'BlurAdmin.pages.avaliacao.grafico.controller'
   ])
-      .config(routeConfig);
+      .config(routeConfig)
+      .config(chartJsConfig);
 
   /** @ngInject */
   function routeConfig($stateProvider) {
@@ -15,6 +16,22 @@
           templateUrl: 'app/pages/avaliacoes/graficos/grafico.html',
           title: 'Resultados'
         });
+  };
+  
+  function chartJsConfig(ChartJsProvider, baConfigProvider) {
+    var layoutColors = baConfigProvider.colors;
+    // Configure all charts
+    ChartJsProvider.setOptions({
+      colours: [ layoutColors.primary, layoutColors.danger, layoutColors.warning, layoutColors.success, layoutColors.info, layoutColors.default, layoutColors.primaryDark, layoutColors.successDark, layoutColors.warningLight, layoutColors.successLight, layoutColors.primaryLight],
+      responsive: true,
+      scaleFontColor: layoutColors.defaultText,
+      scaleLineColor: layoutColors.border,
+      pointLabelFontColor: layoutColors.defaultText
+    });
+    // Configure all line charts
+    ChartJsProvider.setOptions('Line', {
+      datasetFill: false
+    });
   }
 
 })();
